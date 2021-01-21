82da93ee90d757232cd2e19ed25e3b6bfa7007ff
Brice Michallski <brice_michalski@carrefour.com>
2021-01-21 18:58:44 +0100

    Décomposition

--- décomposition/validator.py
+++ décomposition/validator.py
@@ -1,13 +1,10 @@
 import re
 
-def is_present(a):
-    return a is not None
+def is_present(value):
+    return value is not None
 
 def is_valid(value):
-    return True if re.match(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]*\.[a-z]{2,3}$",
-        value
-    ) else False
+    return True if re.match(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9]*\.[a-z]{2,3}$", value) else False
 
 def is_spam(email,spam_list):
     return email.split("@")[1] in spam_list
